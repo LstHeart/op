2d3111388908b444950cd639b8b4d0029b66eee6
LstHeart <1042138926@qq.com>
2021-01-22 03:50:42 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -13,6 +13,7 @@ sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git sirpdboy-package https://github.com/siropboy/sirpdboy-package' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
