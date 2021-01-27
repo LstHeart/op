7248aec7a690f080568e7813230e41afd93a27cb
lstheart <1042138926@qq.com>
2021-01-28 04:12:00 +0800

    脚本配置修复

--- router/phicomm_n1/diy-part2.sh
+++ router/phicomm_n1/diy-part2.sh
@@ -33,11 +33,9 @@ cat >.config <<-EOF
 CONFIG_TARGET_armvirt=y
 CONFIG_TARGET_armvirt_64=y
 CONFIG_TARGET_armvirt_64_Default=y
-
 # 新增或打开需要添加的插件(luci-app)
 # DNS防污染插件
 CONFIG_PACKAGE_luci-app-smartdns=y
-
 # 网络端口转发插件
 CONFIG_PACKAGE_socat=y
 CONFIG_SOCAT_SSL=y
@@ -48,7 +46,6 @@ CONFIG_PACKAGE_luci-app-jd-dailybonus=y
 # 甜糖星愿自动收集插件
 # CONFIG_PACKAGE_luci-app-ttnode=y
 # CONFIG_PACKAGE_luci-app-ttnode is not set
-
 # 网络唤醒wol
 # CONFIG_DEFAULT_luci-app-wol is not set
 CONFIG_PACKAGE_wol=y
@@ -56,11 +53,9 @@ CONFIG_PACKAGE_luci-app-wol=y
 CONFIG_PACKAGE_luci-i18n-wol-en=y
 CONFIG_PACKAGE_luci-i18n-wol-zh-cn=y
 CONFIG_PACKAGE_etherwake=y
-
 # docker界面管理
 CONFIG_PACKAGE_luci-lib-docker=y
 CONFIG_PACKAGE_luci-app-dockerman=y
-
 # 内网穿透Zerotier
 CONFIG_PACKAGE_zerotier=y
 CONFIG_PACKAGE_luci-app-zerotier=y
@@ -71,7 +66,6 @@ CONFIG_PACKAGE_luci-i18n-cpufreq-zh_Hans=y
 # CONFIG_DEFAULT_luci-app-cpufreq is not set
 # samba4
 CONFIG_PACKAGE_luci-app-samba4=y
-
 
 # ---关闭不需要的插件---
 # 解锁网易云音乐
@@ -94,13 +88,10 @@ CONFIG_PACKAGE_luci-app-samba4=y
 # CONFIG_PACKAGE_luci-app-nlbwmon is not set
 # 查看各终端实时流量网速的插件,依赖nlbwmon
 # CONFIG_PACKAGE_luci-app-wrtbwmon=y
-EOF
+
 # ==========luci-theme==========
 # 修改主题配置
-cat >.config <<-EOF
 # CONFIG_PACKAGE_luci-theme-bootstrap is not set
 CONFIG_PACKAGE_luci-theme-argon=y
 CONFIG_PACKAGE_luci-app-argon-config=y
 EOF
-
-make defconfig
