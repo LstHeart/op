65514e6fe04edae1d53489917327c5e1cfd68ecc
lstheart <1042138926@qq.com>
2021-01-30 13:38:07 +0800

    update

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -6,6 +6,7 @@
 # Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
 # Copyright (C) 2020 https://github.com/ophub/op
 # 此脚本用于在 update feeds 之前的相关修改
+#========================================================================================================================
 # cd openwrt
 # other
 # 添加三方包，删除原有包、主题等
@@ -28,15 +29,16 @@
     git clone --depth 1 https://github.com/lisaac/luci-app-dockerman.git
     sudo rm -rf lean/{samba4,luci-app-samba4,luci-theme-argon,luci-lib-docker,qBittorrent}
 )
-# Uncomment a feed source
-# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
-# Repalce a feed source
-# sed -i "s/coolsnowwolf\/luci/Lienol\/openwrt-luci.git;18.06/" ./feeds.conf.default
-# sed -i "s/coolsnowwolf\/packages/Lienol\/openwrt-packages.git;main/" ./feeds.conf.default
+# Change feeds source
+cat >./feeds.conf.default<<-EOF
+# src-git packages https://github.com/coolsnowwolf/packages
+# src-git luci https://github.com/coolsnowwolf/luci
+src-git packages https://github.com/Lienol/openwrt-packages.git;main
+src-git luci https://github.com/luci/Lienol/openwrt-luci.git;18.06
+src-git routing https://git.openwrt.org/feed/routing.git
+src-git telephony https://git.openwrt.org/feed/telephony.git
+src-git freifunk https://github.com/freifunk/openwrt-packages.git
+src-git kenzo https://github.com/kenzok8/openwrt-packages
+src-git small https://github.com/kenzok8/small
+EOF
